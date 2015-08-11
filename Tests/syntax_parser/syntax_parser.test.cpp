08245d5d85bc76bbc06860e022724b8c67519350
James Fowkes <jamesfowkes@gmail.com>
2015-08-11 12:41:57 +0000

    syntax_parser test uses cppunit

--- Tests/syntax_parser/syntax_parser.test.cpp
+++ Tests/syntax_parser/syntax_parser.test.cpp
@@ -6,9 +6,12 @@
 #include <stdint.h>
 #include <iostream>
 
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+
 #include "ast_node.h"
 #include "syntax_parser.h"
-#include "unity.h"
 
 static char const * s_pToTest = NULL;
 bool s_expected = false;
@@ -19,115 +22,136 @@ static bool fFalse(void) { return false; }
 #define RUN_SUCCESS_TEST(arg, expected) \
    s_pToTest = arg; \
    s_expected = expected; \
-   RUN_TEST(TestForParseSuccess);
+   TestForParseSuccess();
 
 #define RUN_FAILURE_TEST(arg) \
    s_pToTest = arg; \
    TestForParseFailure();
 
-void TestForParseSuccess(void)
-{
-   Parser parser;
-   ASTNode * pNode = LEP_Parse(&parser, s_pToTest);
-   TEST_ASSERT_TRUE_MESSAGE(parser.m_success, parser.m_errorMessage);
+class SyntaxParserTest : public CppUnit::TestFixture  {
 
-   bool actual = LEP_Evaluate(pNode);
-   TEST_ASSERT_EQUAL(s_expected, actual);
-}
+   CPPUNIT_TEST_SUITE(SyntaxParserTest);
+   CPPUNIT_TEST(ValidSyntaxTests);
+   CPPUNIT_TEST(InvalidSyntaxTests);
+   CPPUNIT_TEST_SUITE_END();
 
-void TestForParseFailure(void)
-{
-   Parser parser;
-   ASTNode * pNode = LEP_Parse(&parser, s_pToTest);
-   TEST_ASSERT_NULL(pNode);
-   TEST_ASSERT_FALSE(parser.m_success);
-}
+   void TestForParseSuccess(void)
+   {
+      Parser parser;
+      ASTNode * pNode = LEP_Parse(&parser, s_pToTest);
+      CPPUNIT_ASSERT_MESSAGE(parser.m_errorMessage, parser.m_success);
+
+      bool actual = LEP_Evaluate(pNode);
+      CPPUNIT_ASSERT_EQUAL(s_expected, actual);
+   }
+
+   void TestForParseFailure(void)
+   {
+      Parser parser;
+      ASTNode * pNode = LEP_Parse(&parser, s_pToTest);
+      CPPUNIT_ASSERT(!pNode);
+      CPPUNIT_ASSERT(!parser.m_success);
+   }
+
+   void ValidSyntaxTests()
+   {
+      RUN_SUCCESS_TEST("F", false);
+      RUN_SUCCESS_TEST("T", true);
+      RUN_SUCCESS_TEST("0", false);
+      RUN_SUCCESS_TEST("1", true);
+
+      RUN_SUCCESS_TEST("0|0", false);
+      RUN_SUCCESS_TEST("0|1", true);
+      RUN_SUCCESS_TEST("1|0", true);
+      RUN_SUCCESS_TEST("1|1", true);
+     
+      RUN_SUCCESS_TEST("0&0", false);
+      RUN_SUCCESS_TEST("0&1", false);
+      RUN_SUCCESS_TEST("1&0", false);
+      RUN_SUCCESS_TEST("1&1", true);
+
+      RUN_SUCCESS_TEST("F|F", false);
+      RUN_SUCCESS_TEST("F|T", true);
+      RUN_SUCCESS_TEST("T|F", true);
+      RUN_SUCCESS_TEST("T|T", true);
+     
+      RUN_SUCCESS_TEST("F&F", false);
+      RUN_SUCCESS_TEST("F&T", false);
+      RUN_SUCCESS_TEST("T&F", false);
+      RUN_SUCCESS_TEST("T&T", true);
+
+      RUN_SUCCESS_TEST("0|T", true);
+      RUN_SUCCESS_TEST("0|F", false);
+      RUN_SUCCESS_TEST("1|T", true);
+      RUN_SUCCESS_TEST("1|F", true);
+      RUN_SUCCESS_TEST("0&T", false);
+      RUN_SUCCESS_TEST("0&F", false);
+      RUN_SUCCESS_TEST("1&T", true);
+      RUN_SUCCESS_TEST("1&F", false);
+
+      RUN_SUCCESS_TEST("!F", true);
+      RUN_SUCCESS_TEST("!T", false);
+
+      RUN_SUCCESS_TEST("!0", true);
+      RUN_SUCCESS_TEST("!1", false);
+
+      RUN_SUCCESS_TEST("!0|0", true);
+      RUN_SUCCESS_TEST("!0|1", true);
+      RUN_SUCCESS_TEST("!1|0", false);
+      RUN_SUCCESS_TEST("!1|1", true);
+
+      RUN_SUCCESS_TEST("!0&0", false);
+      RUN_SUCCESS_TEST("!0&1", true);
+      RUN_SUCCESS_TEST("!1&0", false);
+      RUN_SUCCESS_TEST("!1&1", false);
+
+      RUN_SUCCESS_TEST("!F|F", true);
+      RUN_SUCCESS_TEST("!F|T", true);
+      RUN_SUCCESS_TEST("!T|F", false);
+      RUN_SUCCESS_TEST("!T|T", true);
+
+      RUN_SUCCESS_TEST("!F&F", false);
+      RUN_SUCCESS_TEST("!F&T", true);
+      RUN_SUCCESS_TEST("!T&F", false);
+      RUN_SUCCESS_TEST("!T&T", false);
+
+      RUN_SUCCESS_TEST("(0)", false);
+      RUN_SUCCESS_TEST("(1)", true);
+
+      RUN_SUCCESS_TEST("(F)", false);
+      RUN_SUCCESS_TEST("(T)", true);
+
+      RUN_SUCCESS_TEST("(1)&(0|1)", true);
+      RUN_SUCCESS_TEST("(1)|(0&1)", true);
+      RUN_SUCCESS_TEST("(0)|(0&1)", false);
+   }
+
+   void InvalidSyntaxTests()
+   {
+      RUN_FAILURE_TEST("   1|2,5");
+      RUN_FAILURE_TEST("   1|2.5e2");
+      RUN_FAILURE_TEST("M1 & 2.5");
+      RUN_FAILURE_TEST("1 | 2.5.6");
+      RUN_FAILURE_TEST("1 || 2.5");
+      RUN_FAILURE_TEST("|1 & 2.5");
+   }
+};
 
 int main()
 {
-   UnityBegin("syntax_parser.test.cpp");
-
    LEP_Init();
 
    LEP_RegisterFunction(0, fFalse);
    LEP_RegisterFunction(1, fTrue);
 
-   RUN_SUCCESS_TEST("F", false);
-   RUN_SUCCESS_TEST("T", true);
-   RUN_SUCCESS_TEST("0", false);
-   RUN_SUCCESS_TEST("1", true);
+   CppUnit::TextUi::TestRunner runner;
+   
+   CPPUNIT_TEST_SUITE_REGISTRATION( SyntaxParserTest );
 
-   RUN_SUCCESS_TEST("0|0", false);
-   RUN_SUCCESS_TEST("0|1", true);
-   RUN_SUCCESS_TEST("1|0", true);
-   RUN_SUCCESS_TEST("1|1", true);
-  
-   RUN_SUCCESS_TEST("0&0", false);
-   RUN_SUCCESS_TEST("0&1", false);
-   RUN_SUCCESS_TEST("1&0", false);
-   RUN_SUCCESS_TEST("1&1", true);
+   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
 
-   RUN_SUCCESS_TEST("F|F", false);
-   RUN_SUCCESS_TEST("F|T", true);
-   RUN_SUCCESS_TEST("T|F", true);
-   RUN_SUCCESS_TEST("T|T", true);
-  
-   RUN_SUCCESS_TEST("F&F", false);
-   RUN_SUCCESS_TEST("F&T", false);
-   RUN_SUCCESS_TEST("T&F", false);
-   RUN_SUCCESS_TEST("T&T", true);
-
-   RUN_SUCCESS_TEST("0|T", true);
-   RUN_SUCCESS_TEST("0|F", false);
-   RUN_SUCCESS_TEST("1|T", true);
-   RUN_SUCCESS_TEST("1|F", true);
-   RUN_SUCCESS_TEST("0&T", false);
-   RUN_SUCCESS_TEST("0&F", false);
-   RUN_SUCCESS_TEST("1&T", true);
-   RUN_SUCCESS_TEST("1&F", false);
-
-   RUN_SUCCESS_TEST("!F", true);
-   RUN_SUCCESS_TEST("!T", false);
-
-   RUN_SUCCESS_TEST("!0", true);
-   RUN_SUCCESS_TEST("!1", false);
-
-   RUN_SUCCESS_TEST("!0|0", true);
-   RUN_SUCCESS_TEST("!0|1", true);
-   RUN_SUCCESS_TEST("!1|0", false);
-   RUN_SUCCESS_TEST("!1|1", true);
-
-   RUN_SUCCESS_TEST("!0&0", false);
-   RUN_SUCCESS_TEST("!0&1", true);
-   RUN_SUCCESS_TEST("!1&0", false);
-   RUN_SUCCESS_TEST("!1&1", false);
-
-   RUN_SUCCESS_TEST("!F|F", true);
-   RUN_SUCCESS_TEST("!F|T", true);
-   RUN_SUCCESS_TEST("!T|F", false);
-   RUN_SUCCESS_TEST("!T|T", true);
-
-   RUN_SUCCESS_TEST("!F&F", false);
-   RUN_SUCCESS_TEST("!F&T", true);
-   RUN_SUCCESS_TEST("!T&F", false);
-   RUN_SUCCESS_TEST("!T&T", false);
-
-   RUN_SUCCESS_TEST("(0)", false);
-   RUN_SUCCESS_TEST("(1)", true);
-
-   RUN_SUCCESS_TEST("(F)", false);
-   RUN_SUCCESS_TEST("(T)", true);
-
-   RUN_SUCCESS_TEST("(1)&(0|1)", true);
-   RUN_SUCCESS_TEST("(1)|(0&1)", true);
-   RUN_SUCCESS_TEST("(0)|(0&1)", false);
-
-   RUN_FAILURE_TEST("   1|2,5");
-   RUN_FAILURE_TEST("   1|2.5e2");
-   RUN_FAILURE_TEST("M1 & 2.5");
-   RUN_FAILURE_TEST("1 | 2.5.6");
-   RUN_FAILURE_TEST("1 || 2.5");
-   RUN_FAILURE_TEST("|1 & 2.5");
+   runner.addTest( registry.makeTest() );
+   runner.run();
 
    return 0;
 }
